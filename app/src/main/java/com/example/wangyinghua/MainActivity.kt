3bdcd70230359504c33ab29fe89d6a6af2eb1f3e
WangYingHua <s1101699@gm.pu.edu.tw>
2023-05-31 10:55:16 +0800

    v.3

--- app/src/main/java/com/example/wangyinghua/MainActivity.kt
+++ app/src/main/java/com/example/wangyinghua/MainActivity.kt
@@ -3,17 +3,23 @@ package com.example.wangyinghua
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.wangyinghua.ui.theme.WangYingHuaTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +40,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Canvas(modifier = Modifier){
+            drawRect(androidx.compose.ui.graphics.Color.Blue, Offset(200f, 300f), Size(100f,100f))
+        }
+        Canvas(modifier = Modifier){
+            drawRect(androidx.compose.ui.graphics.Color.Blue, Offset(500f, 500f), Size(100f,100f))
+        }
+    }
     Column() {
         Text(text = "作者:資管二B 王櫻樺")
         Image(painter = painterResource(id = R.drawable.map), contentDescription = "地圖")
