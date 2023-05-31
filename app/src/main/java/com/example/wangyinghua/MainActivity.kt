13bb5e7c6b1172af9a71ca5ecf5c111af79e706b
WangYingHua <s1101699@gm.pu.edu.tw>
2023-05-31 09:52:27 +0800

    v.2

--- app/src/main/java/com/example/wangyinghua/MainActivity.kt
+++ app/src/main/java/com/example/wangyinghua/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.wangyinghua
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.wangyinghua.ui.theme.WangYingHuaTheme
 
@@ -31,13 +34,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column() {
+        Text(text = "作者:資管二B 王櫻樺")
+        Image(painter = painterResource(id = R.drawable.map), contentDescription = "地圖")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     WangYingHuaTheme {
-        Greeting("Android")
+        //Greeting("Android")
     }
 }
